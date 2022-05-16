03ef16449d273194a369fb7ebbf372c699b12976
davide.trupia <trupid@usi.ch>
2022-05-16 09:17:12 +0200

    Adding TPCDS-KIT in folder

--- docker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run application locally on 8 cores
-./bin/spark-submit \
-  --class org.apache.spark.examples.SparkPi \
-  --master local[8] \
-  /spark-home/examples/jars/spark-examples_2.11-2.4.4.jar 80 \
-  100
